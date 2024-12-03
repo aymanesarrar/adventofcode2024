d8dcb7a64cb5a61b1f56c961ac4ab8ad37bb45ca
aymanesarrar <aymensarrar1@gmail.com>
2024-12-04 00:17:11 +0100

    day 2 part 1

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Setup\ Game/Desktop/projects/adventofcode2024/day2/target/debug/day2: /mnt/c/Users/Setup\ Game/Desktop/projects/adventofcode2024/day2/src/main.rs
---
+++ day2/target/debug/deps/day2-0b312c62a219b418.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Setup Game/Desktop/projects/adventofcode2024/day2/target/debug/deps/day2-0b312c62a219b418: src/main.rs
+
+/mnt/c/Users/Setup Game/Desktop/projects/adventofcode2024/day2/target/debug/deps/day2-0b312c62a219b418.d: src/main.rs
+
+src/main.rs:
